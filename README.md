3449a6e65d6333e8d8ddcdff7da205a8b2e90572
zuuperman <nils.destoop@gmail.com>
2016-10-12 15:30:45 +0200

    Generated angular app + basic integration of prototype

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # projectaanvraag-angular
-The angular application for projectaanvraag
+
+The angular application for projectaanvraag.
+
+## Build & development
+
+Run `grunt` for building and `grunt serve` for preview.
+
+## Testing
+
+Running `grunt test` will run the unit tests with karma.
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name projectaanvraagAngularApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the projectaanvraagAngularApp
+ */
+angular.module('projectaanvraagAngularApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
