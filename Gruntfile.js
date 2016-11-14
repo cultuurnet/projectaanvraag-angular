079b257a7817e41ad640bb385e14b9712d2a3a4a
zuuperman <nils.destoop@gmail.com>
2016-11-14 11:21:53 +0100

    Change coveralls config

--- Gruntfile.js
+++ Gruntfile.js
@@ -17,7 +17,7 @@ module.exports = function (grunt) {
     useminPrepare: 'grunt-usemin',
     ngtemplates: 'grunt-angular-templates',
     cdnify: 'grunt-google-cdn',
-    karma: 'coveralls',
+    coveralls: 'grunt-karma-coveralls',
     ngconstant: 'grunt-ng-constant'
   });
 
