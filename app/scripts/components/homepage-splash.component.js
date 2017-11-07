53148f1e97420375f6b712fcf4771b0c90bc0b50
Reinout Bossuyt <reinout@cultuurnet.be>
2017-11-07 11:29:28 +0100

    PROJ-60 update

--- app/scripts/components/homepage-splash.component.js
+++ app/scripts/components/homepage-splash.component.js
@@ -10,7 +10,6 @@ angular
 /* @ngInject */
 function projectaanvraagHomepageSplashController() {
   function putContact() {
-    console.log(Snap.select('.contact_r').getBBox());
     Snap.select('.contact_r').animate({transform: 'r0,2108,325t-24,13'}, 1500, mina.linear, lights);
   }
 
