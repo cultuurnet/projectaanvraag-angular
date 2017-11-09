53c08bb807d4624fbfae9253f3d3cb4e1b84d020
Reinout Bossuyt <reinout@cultuurnet.be>
2017-11-09 19:33:04 +0100

    PROJ-60 update

--- app/scripts/components/homepage-splash.component.js
+++ app/scripts/components/homepage-splash.component.js
@@ -9,17 +9,26 @@ angular
 
 /* @ngInject */
 function projectaanvraagHomepageSplashController() {
+  /* jshint ignore:start */
+  var s = Snap('.hero-image');
+  Snap.load('images/hero.svg', onSVGLoaded);
+
+  function onSVGLoaded(data){
+      s.append(data);
+  }
+
   function putContact() {
-    /* jshint ignore:start */
-    Snap.select('.contact_r').animate({transform: 'r0,2108,325t-24,13'}, 1500, mina.linear, function(){
+
+    Snap.select('.contact_r').animate({transform: 'r0,2108,325t-24,13'}, 1200, mina.linear, function(){
       Snap.select('.letters').addClass('flicker-in-2');
       Snap.select('.been').addClass('flicker-in-2');
     });
-    /* jshint ignore:end */
+
   }
 
   setTimeout(function() {
     putContact();
   }, 1500);
+   /* jshint ignore:end */
 
 }
