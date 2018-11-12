b957e0a7bed078e05e4a3b86ee0439a886c703d7
Reinout Bossuyt <reinout@publiq.be>
2018-11-12 10:26:32 +0100

    PROJ-87

--- app/scripts/components/add_project.component.js
+++ app/scripts/components/add_project.component.js
@@ -44,31 +44,34 @@
           delete ctrl.formData.coupon;
         }
 
-        ctrl.formData.integrationType = ctrl.formData.integration.id;
+        if(ctrl.formData.integration) {
 
-        ctrl.saving = true;
+          ctrl.formData.integrationType = ctrl.formData.integration.id;
 
-        projectaanvraagApiService.addProject(ctrl.formData).then(function() {
-          // Show success message and redirect to the dashboard
-          Messages.addMessage('success', 'Je project is aangemaakt. Je vindt het hieronder in de lijst terug.');
-          ctrl.saving = false;
-          $state.go('authenticated.dashboard');
-        }, function(result) {
+          ctrl.saving = true;
 
-          // Show error label, if the code is known.
-          if (result && apiErrorCodes[result.code]) {
-            Messages.addMessage('danger', apiErrorCodes[result.code].label);
-          }
-          else {
-            Messages.addMessage('danger', 'Er ging iets mis. Probeer het later opnieuw.');
-          }
+          projectaanvraagApiService.addProject(ctrl.formData).then(function() {
+            // Show success message and redirect to the dashboard
+            Messages.addMessage('success', 'Je project is aangemaakt. Je vindt het hieronder in de lijst terug.');
+            ctrl.saving = false;
+            $state.go('authenticated.dashboard');
+          }, function(result) {
 
-          $location.hash('messages');
-          $anchorScroll();
+            // Show error label, if the code is known.
+            if (result && apiErrorCodes[result.code]) {
+              Messages.addMessage('danger', apiErrorCodes[result.code].label);
+            }
+            else {
+              Messages.addMessage('danger', 'Er ging iets mis. Probeer het later opnieuw.');
+            }
 
-          ctrl.saving = false;
+            $location.hash('messages');
+            $anchorScroll();
 
-        });
+            ctrl.saving = false;
+
+          });
+        }
       } else {
         Messages.addMessage('danger', 'Gelieve de verplichte velden in te vullen.');
       }
