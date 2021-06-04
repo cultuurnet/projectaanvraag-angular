c30989b53efa2b1f8c91afdc315b5c0425d56676
Luc Wollants <lucwollants@gmail.com>
2021-06-04 13:04:25 +0200

    Keep track of opportunity

--- app/scripts/factories/project.factory.js
+++ app/scripts/factories/project.factory.js
@@ -46,6 +46,7 @@ function cultuurnetProjectFactory(IntegrationType, ProjectStatuses) {
       this.totalWidgets = jsonObject.totalWidgets || 0;
       this.sapiVersion = jsonObject.sapiVersion || '2';
       this.contentFilter = jsonObject.contentFilter || '';
+      this.opportunity = false;
 
       this.insightlyUrl = 'https://crm.na1.insightly.com/details/Project/' + this.insightlyProjectId;
       if (jsonObject.projectIdInsightly) {
@@ -53,6 +54,7 @@ function cultuurnetProjectFactory(IntegrationType, ProjectStatuses) {
       }
       if (jsonObject.opportunityIdInsightly && !jsonObject.projectIdInsightly) {
         this.insightlyUrl = 'https://crm.na1.insightly.com/details/Opportunity/' + jsonObject.opportunityIdInsightly;
+        this.opportunity = true;
       }
 
       if (jsonObject.status && ProjectStatuses[jsonObject.status.toUpperCase()]) {
